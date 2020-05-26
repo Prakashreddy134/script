e1b6f63919e4d88b59727caadb55befd011d0ea9
Prakashreddy134 <34908499+Prakashreddy134@users.noreply.github.com>
2020-05-26 13:15:36 +0530

    Create if.sh

---
+++ if.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#set -x
+
+REPO_LIST="ssh repo_jan test"
+
+get_latest_commit (){
+    REPO_NAME=$1
+    curl https://api.github.com/repos/Prakashreddy134/$REPO_NAME/commits/master | \
+        grep sha | \
+        awk '{print $2}'| \
+        sed -n '1'p | \
+        sed 's/"//g'| \
+        sed 's/,//g'
+}
+
+compare_the_commits () {
+        if [ "`cat $FILE`" = "`get_latest_commit $1`" ]
+        then
+            echo "There are no changes in $1 repo"
+        else
+            STATUS=changed
+            get_latest_commit $1 > $FILE
+        fi
+}
+
+main () {
+for i in $REPO_LIST
+do
+    FILE=/tmp/${i}_latest_commit.txt
+    if [ ! -f $FILE ]
+    then
+        touch $FILE
+    fi
+
+    compare_the_commits $i
+
+    if [ "$STATUS" = "changed" ]
+    then
+        if [ "$i" = "test" ]
+        then
+           URL=https://github.com/Prakashreddy134/test/blob/master/test.txt
+           job=test-deploy-job
+           echo "$URL"
+           echo "$job"
+       echo "There are changes in $i repo"
+       fi
+    fi
+done
+}
+
+main
